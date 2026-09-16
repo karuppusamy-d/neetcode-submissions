20ae28d7fa7db0f6880fa1288d245876efde95ed
Karuppusamy <d.karuppusamy@outlook.com>
2026-09-16 23:07:04 +0530

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const set = new Set<number>(nums);
+
+        let longest = 0;
+        for (let num of set) {
+            if (!set.has(num-1)) {
+                let len = 1;
+                while (set.has(++num)) {
+                    len++;
+                }
+                longest = Math.max(len, longest)
+            }
+            
+        }
+
+        return longest;
+    }
+}
