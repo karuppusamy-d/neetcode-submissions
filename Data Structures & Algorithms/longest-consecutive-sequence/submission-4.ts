9683796ed150e9717a50621558af43144e800af4
Karuppusamy <d.karuppusamy@outlook.com>
2026-09-16 23:02:36 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const set = new Set<number>(nums);
+
+      
+        const seq = new Set<number>();
+        for (let num of set) {
+            if (!set.has(num-1)) {
+                seq.add(num);
+            }
+        }
+
+        let longest = 0;
+        for (let num of seq) {
+            let len = 0;
+            let itr = num;
+            while (set.has(itr)) {
+                len++;
+                itr++;
+            }
+            longest = Math.max(len, longest)
+        }
+        
+
+        return longest;
+    }
+}
