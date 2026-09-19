2ee3b8e33232bc35bd3da2e3569fd661949231a2
Karuppusamy <d.karuppusamy@outlook.com>
2026-09-20 02:54:27 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const stack: string[] = [];
+        for (const char of s) {
+            switch(char) {
+                case "(":
+                case "[":
+                case "{":
+                    stack.push(char)
+                    break;
+                case ")":
+                    if (stack.pop() !== "(") return false;
+                    break;
+                case "]":
+                    if (stack.pop() !== "[") return false;
+                    break;
+                case "}":
+                    if (stack.pop() !== "{") return false;
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
