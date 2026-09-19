d1214597395a0b3da83a3083748c126ec6ab63b6
Karuppusamy <d.karuppusamy@outlook.com>
2026-09-20 02:45:43 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let max=0;
+
+        let left = 0;
+        let right = heights.length - 1;
+        while (left < right) {
+            const width = right - left;
+            const volume = Math.min(heights[left], heights[right]) * width
+            if (volume > max) max = volume;
+            if (heights[left] < heights[right]) {
+                left++
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+}
