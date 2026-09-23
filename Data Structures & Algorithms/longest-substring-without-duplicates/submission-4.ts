b02376a4ce6a466df648140a704596131744ef0e
Karuppusamy <d.karuppusamy@outlook.com>
2026-09-24 01:46:29 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        const n = s.length;
+
+        const arr = Array.from({ length: 256 }).fill(false);
+        const a = "a".charCodeAt(0);
+
+        let longest = 0;
+        let left = 0;
+        for (let right = 0; right < n; right++) {
+            const curr = s.charCodeAt(right) - a;
+            
+            longest = Math.max(longest, right - left - 1);
+            while (arr[curr]) {
+                arr[s.charCodeAt(left) - a] = false;
+                left++;
+            }
+            arr[curr] = true;
+            
+            longest = Math.max(longest, right - left + 1);
+        }
+
+        return longest;
+    }
+}
